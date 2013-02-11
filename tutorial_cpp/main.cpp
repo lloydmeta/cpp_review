fae38f9fda7ea463dcb579281c0da6c7a8395514
Lloyd <lloydmeta@gmail.com>
2013-02-11 19:23:31 +0900

    Initial Commit

---
+++ tutorial_cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tutorial_cpp
+//
+//  Created by A13075 on 2013/02/11.
+//  Copyright (c) 2013年 A13075. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
